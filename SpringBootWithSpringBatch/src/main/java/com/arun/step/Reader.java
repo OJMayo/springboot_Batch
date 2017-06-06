2a739d40c2d6f39df76b48d64f9bce100e08a941
arun789 <adwiti1975@gmail.com>
2017-06-06 00:55:22 -0700

    step 1 Reader added

---
+++ SpringBootWithSpringBatch/src/main/java/com/arun/step/Reader.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package com.arun.step;
+
+import java.util.List;
+
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.NonTransientResourceException;
+import org.springframework.batch.item.ParseException;
+import org.springframework.batch.item.UnexpectedInputException;
+
+/**
+ * @author Adwiti
+ *
+ */
+public class Reader implements ItemReader<String> {
+
+	private String[] names = { "arun", "adwiti", "pushpa" };
+	private int count;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.springframework.batch.item.ItemReader#read()
+	 */
+	@Override
+	public String read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+
+		if (count < names.length) {
+			return names[count++];
+		} else {
+			count = 0;
+		}
+
+		return null;
+	}
+
+}
