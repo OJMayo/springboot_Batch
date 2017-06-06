a184c4f9ed76449099ce13c28c6fdf747398fd19
arun789 <adwiti1975@gmail.com>
2017-06-06 01:27:32 -0700

    sprin batch

---
+++ SpringBootWithSpringBatch/src/main/java/com/arun/main/SpringBatchApplication.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.arun.main;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Adwiti
+ *
+ */
+@SpringBootApplication
+@EnableBatchProcessing
+public class SpringBatchApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchApplication.class, args);
+	}
+}
